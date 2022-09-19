b1f9bd1e9f27d02947130fd8fd703de536ede8e7
Esther Csoke <esther.e.csoke@gmail.com>
2022-09-19 12:17:05 +1000

    added library import for pacman

--- Code/model.R
+++ Code/model.R
@@ -1,10 +1,20 @@
+# import data-loader file
 source("Code/data-loader.R")
+
+# install devtools
 install.packages("devtools")
-library(devtools)
+
+# install dev pacman library
 install_github("trinker/pacman")
 
-p_load("modelr")
+# load libraries
+library(devtools)
+library(pacman)
 
+# load in packages
+pacman::p_load("modelr")
+
+# laod dataset
 research_data <- load_datasets()
 
 crossv_kfold(research_data, k = 3)
